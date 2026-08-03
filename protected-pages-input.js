98cdc8e2984ca6b272ca83c0046ec597e15e4d6c
Benoit Guyot <benoit.guyot@monlycee.net>
2026-08-03 11:25:37 +0200

    Ajout d'informations sur l'auteur dans la page de naturalisme, mise à jour des liens dans la liste de contenus, amélioration et complément de la présentation de la page d'accueil et des boutons de navigation, et ajustement du style de navigation. Légères modifications dans le code.

--- protected-pages-input.js
+++ protected-pages-input.js
@@ -14,38 +14,3 @@ var protectedPages = [
                 'contents/Galerie/galerie'
                 //Compléter cette liste au besoin
             ];
-
-document.getElementById('b-acces').addEventListener('click', function () {
-    var code = document.getElementById('acces').value.trim();
-
-    if (!code) {
-        window.location.href = 'erreur-systeme.html';
-        return;
-    }
-
-    if (protectedPages.indexOf(code) !== -1) {
-        window.location.href = 'erreur-systeme.html';
-        return;
-    }
-
-    var targetUrl = code + '.html';
-
-    fetch(targetUrl, { method: 'GET' })
-    .then(function (response) {
-        if (response.ok && protectedPages.indexOf(code) === -1) {
-            window.location.href = targetUrl;
-        } else {
-            window.location.href = 'erreur-systeme.html';
-        }
-    })
-    .catch(function () {
-        window.location.href = 'erreur-systeme.html';
-    });
-});
-
-document.getElementById('acces').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        document.getElementById('b-acces').click();
-    }
-});
